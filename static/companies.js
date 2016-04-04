e67eb4186f16a249a2d4adc168e023fb8edef69a
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-04-04 20:22:51 +0200

    Use local images

--- static/companies.js
+++ static/companies.js
@@ -1,25 +1,24 @@
 export default [{
   name: 'Runtastic',
-  _logo: 'https://www.onedayonly.co.za/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/1/6/162425-runtastic_logo_online-a6159f-large-1428579326.png',
   logo: 'static/images/runtastic_logo.png',
   duties: '',
   stack: 'Ember.js - Leaflet - Ember-cli - Tape.js - Broccoli.js - ES6/ES7 - S3 - ServiceWorkers - WebRTC - JSON-Api - Ruby on Rails - Bugsnag',
   site: 'https://www.runtastic.com/'
 }, {
   name: 'Influencity',
-  logo: 'http://plugandplayspain.es/wp-content/uploads/2014/10/influencity.png',
+  logo: 'static/images/influencity_logo.png',
   duties: '',
   stack: 'Node.js - Restify - PM2 - ec2 - Superagent - Lodash - Sequelize - PostgreSQL - Jsdom - Twitter Api - Instagram Api',
   site: 'https://influencity.com/'
 }, {
   name: 'King',
-  logo: 'https://upload.wikimedia.org/wikipedia/en/8/8e/King_logo_2013.png',
+  logo: 'static/images/king_logo.png',
   duties: '',
   stack: 'React.js - Babel.js - Angular.js - Webpack - Gulp - Web components - Sinon - Mocha - Chai.js - Moment.js - Browserify',
   site: 'https://king.com/'
 }, {
   name: 'Kerad Games',
-  logo: 'https://confly.s3.amazonaws.com/uploads/sponsor/formatted_logo_v2/25/keard_logo.png',
+  logo: 'static/images/kerad_logo.png',
   duties: '',
   stack: 'Ember.js - SASS - Grunt - i18n.js - Handlebars.js - RSVP.js - FB sdk - Cloudfront - Sentry - Karma - QUnit.js',
   site: 'http://www.keradgames.com/'
