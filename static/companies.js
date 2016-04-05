df35bae4dd463f042637915d5a65345ad34470b1
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-04-05 23:32:24 +0200

    Re-enable repositories

--- static/companies.js
+++ static/companies.js
@@ -1,25 +1,25 @@
 export default [{
   name: 'Runtastic',
   logo: 'static/images/runtastic_logo.png',
-  duties: '',
-  stack: 'Ember.js - Leaflet - Ember-cli - Tape.js - Broccoli.js - ES6/ES7 - S3 - ServiceWorkers - WebRTC - JSON-Api - Ruby on Rails - Bugsnag',
+  duties: 'Our goal was to improve the overal performance of the web client, for make that possible, we mostly used all the existing techniques and good practices like assets minification, concatenation, http cache, service workers, critical css path, inial server-side rendering, lazy load resources, image optimization...',
+  stack: 'Ember.js - Leaflet - Ember-cli - Tape.js - Broccoli.js - ES6/ES7 - S3 - ServiceWorkers - WebRTC - JSON-Api - Ruby on Rails - Bugsnag.',
   site: 'https://www.runtastic.com/'
 }, {
   name: 'Influencity',
   logo: 'static/images/influencity_logo.png',
-  duties: '',
+  duties: 'I was working as contractor in a standalone project, a http microservice for provide statistics from Twitter and Instagram. During that time I was in charge ',
   stack: 'Node.js - Restify - PM2 - ec2 - Superagent - Lodash - Sequelize - PostgreSQL - Jsdom - Twitter Api - Instagram Api',
   site: 'https://influencity.com/'
 }, {
   name: 'King',
   logo: 'static/images/king_logo.png',
-  duties: '',
-  stack: 'React.js - Babel.js - Angular.js - Webpack - Gulp - Web components - Sinon - Mocha - Chai.js - Moment.js - Browserify',
+  duties: 'Moving internally King applications used by games like Candy Crush, Pet Rescue or Farm heroes to the next level of web clients. Due the size of the company and diversity of projects we had to unify the way of building frontend, in order to achieve that we defined a style guide, common subset of UI components using React and several cli tools for improve the developer experience.',
+  stack: 'React.js - Babel.js - ESLint - Angular.js - Webpack - Gulp - Web components - Sinon - Mocha - Chai.js - Moment.js - Browserify - Commander.js',
   site: 'https://king.com/'
 }, {
   name: 'Kerad Games',
   logo: 'static/images/kerad_logo.png',
-  duties: '',
-  stack: 'Ember.js - SASS - Grunt - i18n.js - Handlebars.js - RSVP.js - FB sdk - Cloudfront - Sentry - Karma - QUnit.js',
+  duties: 'Build the v2 of GoldenManager, an online football manager game with more than 4M users, unique UIX and real time bidding system. We achieved the 100% migration of the frontend code from the Rails monolithic app, converting the client into a SPA and improving the game experience.',
+  stack: 'Ember.js - Websockets - SASS - Grunt - i18n.js - Handlebars.js - RSVP.js - FB sdk - Cloudfront - Sentry - Karma - QUnit.js',
   site: 'http://www.keradgames.com/'
 }];
